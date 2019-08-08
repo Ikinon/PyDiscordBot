a5aeeacca93d773f1b610b743e8d45b6350b7b46
Ikinon <36565735+Ikinon@users.noreply.github.com>
2019-08-08 18:28:12 +0100

    Add util for Errors
    Catch common errors and pass
    Reply with traceback if its not known

---
+++ PyDiscordBot/utils/ErrorUtils.py
@@ -0,0 +1,39 @@
+import sys
+import traceback
+
+from aiohttp import ClientSession
+from discord.ext import commands
+
+
+class ErrorUtils(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        """Triggered when as an error is raised processing a command."""
+
+        if hasattr(ctx.command, 'on_error'):
+            return
+
+        ignored = (commands.CommandNotFound, commands.UserInputError)
+        error = getattr(error, 'original', error)
+
+        if isinstance(error, ignored):
+            return
+
+        elif isinstance(error, commands.DisabledCommand):
+            return await ctx.send(f'{ctx.command} has been disabled.')
+
+        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+        traceback.print_exception(type(error), error, error.__traceback__)
+        err = traceback.format_exception(type(error), error, error.__traceback__)
+        async with ClientSession() as session:
+            async with session.post('https://hasteb.in/documents', data=err[0].encode('utf-8')) as post:
+                return await ctx.send(
+                    "There was an error while running the command!\nStack Trace: "'https://hasteb.in/' +
+                    (await post.json())['key'])
+
+
+def setup(bot):
+    bot.add_cog(ErrorUtils(bot))
