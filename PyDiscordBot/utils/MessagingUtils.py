0a5c0ddcda7bdd1b96feeb4803130e766d72be5a
Ikinon <36565735+Ikinon@users.noreply.github.com>
2019-12-10 22:18:39 +0000

    Create function for timeout messages

--- PyDiscordBot/utils/MessagingUtils.py
+++ PyDiscordBot/utils/MessagingUtils.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import discord
 
 
@@ -5,10 +7,27 @@ async def convert(ctx):
     return f"Requested by {ctx.author}"
 
 
-async def embed_basic(ctx, title, description, colour, footer: bool, footercontent= None):
+async def embed_basic(ctx, title, description, colour, footer: bool, footercontent=None):
     embed = discord.Embed(title=title, description=description, colour=colour)
     if footer is True:
         if footercontent is None:
-            footercontent= await convert(ctx)
+            footercontent = await convert(ctx)
         embed.set_footer(text=footercontent)
     return embed
+
+
+async def message_timechecked(bot, ctx, msg, timeout):
+    await msg.add_reaction('✅')
+    await msg.add_reaction('❌')
+
+    def check(reaction, user):
+        if user == ctx.author and '✅' in str(reaction):
+            return True
+        if user == ctx.author and '❌' in str(reaction):
+            raise asyncio.TimeoutError
+
+    try:
+        msg.reactions, user = await bot.wait_for('reaction_add', timeout=timeout, check=check)
+        return True
+    except asyncio.TimeoutError:
+        return False
