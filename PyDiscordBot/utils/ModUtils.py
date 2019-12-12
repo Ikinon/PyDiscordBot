b11e5baf63316426091d3ba0e9167fd813022608
Ikinon <36565735+Ikinon@users.noreply.github.com>
2019-12-12 21:04:49 +0000

    Modlog support
    Some database naming cleanup

--- PyDiscordBot/utils/ModUtils.py
+++ PyDiscordBot/utils/ModUtils.py
@@ -32,6 +32,33 @@ async def runchecks(bot, ctx, target):
         return True
 
 
+async def modlog(ctx, target, reason):
+    if await check_modlog_status(ctx) is False:
+        return
+    elif await check_modlog_status(ctx):
+        embed = await MessagingUtils.embed_basic(ctx, f"{ctx.command}ed member",
+                                                 f"{ctx.author.mention} {ctx.command}ed member {target}",
+                                                 Constants.commandInfo, False)
+        embed.add_field(name="Channel", value=f"{ctx.channel.name} ({ctx.channel.id})")
+        embed.add_field(name="Reason", value=reason)
+        modlog = await check_modlog_status(ctx)
+        channel = discord.utils.get(ctx.guild.channels, id=int(modlog[0]))
+        if modlog[1] == "ALL":
+            await channel.send(embed=embed)
+        if str(ctx.command) in modlog[1]:
+            await channel.send(embed=embed)
+
+
+async def check_modlog_status(ctx):
+    for x in DataUtils.database().find(dict({'_id': ctx.guild.id})): x
+    if x.get('modlog_status') == "NONE":
+        return False
+    elif x.get('modlog_status'):
+        temp = [x.get("modlog_channel")]
+        temp.append(x.get("modlog_status"))
+        return temp
+
+
 async def create_role(ctx, name, permissions: discord.permissions, reason=None):
     return await ctx.guild.create_role(name=name, permissions=permissions, reason=reason)
 
@@ -42,6 +69,7 @@ async def kick(bot, ctx, member: discord.Member, reason):
         await member.kick(reason=reason)
         await ctx.send(embed=await MessagingUtils.embed_basic(ctx, f"Kicked member", f"{member} has been kicked!",
                                                               Constants.commandSuccess, True))
+        await modlog(ctx, member, reason)
 
 
 async def ban(bot, ctx, member: discord.Member, reason):
@@ -50,14 +78,17 @@ async def ban(bot, ctx, member: discord.Member, reason):
         await member.ban(reason=reason)
         await ctx.send(embed=await MessagingUtils.embed_basic(ctx, f"Banned member", f"{member} has been banned!",
                                                               Constants.commandSuccess, True))
+        await modlog(ctx, member, reason)
 
 
 async def softban(bot, ctx, member: discord.Member, reason):
     if await runchecks(bot, ctx, member.id):
         reason = await convert(ctx, reason)
-        await member.ban(reason=reason), member.unban(reason=reason)
+        await member.ban(reason=reason), await member.unban(reason=reason)
         await ctx.send(
-            embed=await MessagingUtils.embed_basic(ctx, f"Soft-banned member", f"{member} has been soft-banned!"))
+            embed=await MessagingUtils.embed_basic(ctx, f"Soft-banned member", f"{member} has been soft-banned!",
+                                                   Constants.commandSuccess, True))
+        await modlog(ctx, member, reason)
 
 
 async def unban(ctx, user, reason):
@@ -69,8 +100,7 @@ async def unban(ctx, user, reason):
             await ctx.send(
                 embed=await MessagingUtils.embed_basic(ctx, f"Unbanned User", f"{ban.user} has been unbanned",
                                                        Constants.commandSuccess, True))
-    else:
-        await MessagingUtils.embed_commandFail(ctx, f"Could not find user {str(user)} in ban list!")
+    await modlog(ctx, user, reason)
 
 
 # TODO: Timed mute
@@ -81,13 +111,13 @@ async def mute(bot, ctx, member, reason):
                                                  Constants.commandSuccess, True)
         for x in DataUtils.database().find(): x
         try:
-            role = discord.utils.get(ctx.guild.roles, id=int(x.get('mute role')))
+            role = discord.utils.get(ctx.guild.roles, id=int(x.get('mute_role')))
             if role is None: raise AttributeError
         except AttributeError:
             if ctx.guild.me.guild_permissions.manage_channels is False:
                 raise discord.ext.commands.BotMissingPermissions(['manage_channels'])
             role = await create_role(ctx, "Muted", discord.Permissions(permissions=0))
-            DataUtils.database().update_one(dict({'_id': ctx.guild.id}), dict({'$set': {"mute role": str(role.id)}}))
+            DataUtils.database().update_one(dict({'_id': ctx.guild.id}), dict({'$set': {"mute_role": str(role.id)}}))
             failed = 0
             for channel in ctx.guild.channels:
                 try:
@@ -106,3 +136,4 @@ async def mute(bot, ctx, member, reason):
                 await member.edit(mute=True)
                 embed.add_field(name="Voice Mute", value=f"{member} has also been muted in voice.")
         await ctx.send(embed=embed)
+        await modlog(ctx, member, reason)
