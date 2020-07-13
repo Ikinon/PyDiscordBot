a2e600983c962bd90b5e587e29fe94aa5b71a157
Ikinon <36565735+Ikinon@users.noreply.github.com>
2020-07-13 15:09:49 +0100

    A lot of cleanup

--- PyDiscordBot/commands/Management.py
+++ PyDiscordBot/commands/Management.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 
-from PyDiscordBot.utils import DataUtils, MessagingUtils, ModUtils
+from PyDiscordBot.utils import DataUtils, MessagingUtils, ModUtils, DataUtils
 
 
 class Management(commands.Cog):
@@ -10,31 +10,19 @@ class Management(commands.Cog):
     @commands.command()
     @commands.guild_only()
     @commands.has_permissions(administrator=True)
-    async def settings(self, ctx, settingChange=None, value=None):
+    async def settings(self, ctx, setting=None, value=None):
         blacklist = ['_id', 'guild_id', 'warnings', 'modlog_status', 'modlog_channel']
-        settings = []
+        guildSettings = []
         for x in DataUtils.guilddata(ctx.guild.id):
             if x not in blacklist:
-                settings.append(x)
-        if not settingChange:
+                guildSettings.append(x)
+        if not setting:
             embed = await MessagingUtils.embed_commandInfo(ctx, f"Settings for guild {ctx.guild}", "")
-            for item in settings:
+            for item in guildSettings:
                 embed.add_field(name=item, value=DataUtils.guilddata(ctx.guild.id).get(item), inline=False)
             await ctx.send(embed=embed)
-        # Todo: Check if setting is correct type 
         else:
-            if settingChange in blacklist:
-                await MessagingUtils.send_embed_commandWarning(ctx, "Setting Change",
-                                                               "You are not allowed to modify this setting.")
-            if settingChange not in settings and settingChange not in blacklist:
-                await MessagingUtils.send_embed_commandFail(ctx, "Setting Change", f"{settingChange} does not exist.")
-            if settingChange in settings and settingChange not in blacklist:
-                previous = DataUtils.guilddata(ctx.guild.id).get(settingChange)
-                DataUtils.database().update_one(dict({'_id': ctx.guild.id}), dict({'$set': {settingChange: value}}))
-                embed = await MessagingUtils.embed_commandSuccess(ctx, "Setting Change", "New setting applied")
-                embed.add_field(name=f"Old setting for {settingChange}", value=previous, inline=False)
-                embed.add_field(name=f"New setting for {settingChange}", value=value, inline=False)
-                await ctx.send(embed=embed)
+            await DataUtils.settingChanger(ctx, blacklist, guildSettings, setting, value)
 
     @commands.command()
     @commands.guild_only()
