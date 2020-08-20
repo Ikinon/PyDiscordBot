feca44f2b660200c7ed23f25572462351369227c
Ikinon <36565735+Ikinon@users.noreply.github.com>
2020-08-20 13:24:28 +0100

    reaction checking (numbers)

--- PyDiscordBot/utils/MessagingUtils.py
+++ PyDiscordBot/utils/MessagingUtils.py
@@ -1,6 +1,8 @@
 import asyncio
+import re
 
 import discord
+from discord.ext import commands
 
 from PyDiscordBot.misc import Constants
 
@@ -28,6 +30,49 @@ async def message_timechecked(bot, ctx, msg, timeout) -> bool:
         msg.reactions, user = await bot.wait_for('reaction_add', timeout=timeout, check=check)
         return True
     except asyncio.TimeoutError:
+        return False
+
+
+async def message_timecheckednumbers(bot: commands.Bot, ctx: commands.Context, msg: discord.Message, nummax: int,
+                                     timeout: int, remove_after: bool = False):
+    """
+
+    :param bot: bot instance
+    :param ctx: command context
+    :param msg: msg to add/check reactions on
+    :param nummax: max emoji's to check on (limit: 9)
+    :param timeout: timeout on author clicking
+    :param remove_after: remove the reactions after check complete
+    """
+    if nummax > 9:
+        raise ValueError("argument 'nummax' should be lower than 9")
+    temp = 1
+    for x in range(0, nummax):
+        await msg.add_reaction(f'{temp}\N{combining enclosing keycap}')
+        temp += 1
+
+    def check_reaction(reaction):
+        search = re.search("[0-9](?:\\N{combining enclosing keycap})", str(reaction))
+        if search:
+            return search.group()[0]
+
+    def check(reaction, user):
+        if user == ctx.author:
+            return check_reaction(reaction)
+
+    async def remove_reactions(ctx, m):
+        m = await ctx.fetch_message(m.id)
+        if m.channel.permissions_for(ctx.me).manage_messages:
+            await m.clear_reactions()
+        for reaction in m.reactions:
+            await reaction.remove(ctx.me)
+
+    try:
+        msg.reactions, user = await bot.wait_for('reaction_add', timeout=timeout, check=check)
+        asyncio.get_event_loop().create_task(remove_reactions(ctx, msg))
+        return int(check_reaction(msg.reactions))
+    except asyncio.TimeoutError:
+        asyncio.get_event_loop().create_task(remove_reactions(ctx, msg))
         return False
 
 
